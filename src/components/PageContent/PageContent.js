07594a46d82aeeaa4ceb53ac122bb22b78523563
Meghashree-22 <98081012+Meghashree-22@users.noreply.github.com>
2023-08-16 22:41:43 +0530

    Revert "User dashboard"

--- src/components/PageContent/PageContent.js
+++
@@ -1,9 +0,0 @@
-import AppRoute from "../AppRoute/AppRoute";
-
-function PageContent() {
-    return <div className="PageContent">
-        <AppRoute />
-    </div>;
-
-}
-export default PageContent;
